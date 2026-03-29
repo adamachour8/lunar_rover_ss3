a45dd761cd076bd704b6f9c8aac556cbf242c520
Adam Achour <adamachour8@gmail.com>
2026-03-29 14:59:52 -0400

    petits changements

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# IMPORTS -------------------------------------------------------------------
-
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -14,26 +12,25 @@ import matplotlib.pyplot as plt
 
 # VARIABLES -----------------------------------------------------------------
 
-THRESHOLD = 0.15 # Distance du plan pour que le point soit considéré comme en faisant partie ou non
-NOM_FICHIER = "NuagePtsTest1-6.csv" # Nom du fichier du nuage de points (dans "simulation")
-EPS = 0.25 # Distance d'un point à un autre pour considérer qu'il s'agit du même cluster
-MIN_SAMPLES = 8 # Minimum de points pour être considéré comme un cluster
+THRESHOLD = 0.08
+NOM_FICHIER = "NuagePtsTest1-6.csv"
+EPS = 0.25
+MIN_SAMPLES = 8
 
 # PROGRAMME -----------------------------------------------------------------
 
 points = generer_terrain("simulation/" + NOM_FICHIER)
-obstacles, sol, irregulier, _ = ransac(points, THRESHOLD)
-clusters = dbscan(obstacles, EPS, MIN_SAMPLES)
+terrain_non_navigable, sol, _ = ransac(points, THRESHOLD)
+clusters = dbscan(terrain_non_navigable, EPS, MIN_SAMPLES)
+objets_interet, obstacles = filtrer(clusters)
 
+for obj in objets_interet:
+    print(obj)
+for obj in obstacles:
+    print(obj)
 
-# TESTS (TEMPORAIRE) --------------------------------------------------------
+# PLOT ----------------------------------------------------------------------
 
-# #Imprimer en ordre sexy (DBSCAN)
-# for label, cluster_points in clusters.items():
-#     print(f"Cluster {label}: {len(cluster_points)} points")
-#     print(cluster_points)
-
-# Tester graphiquement (RANSAC) + DBSCAN
 ranges = np.array([
     points[:,0].max() - points[:,0].min(),
     points[:,1].max() - points[:,1].min(),
@@ -49,11 +46,3 @@ ax.set_ylabel('Y')
 ax.set_zlabel('Z')
 ax.set_box_aspect(ranges)
 plt.show()
-
-# après dbscan :
-objets_interet, obstacles = filtrer(clusters)
-
-for obj in objets_interet:
-    print(obj)
-for obj in obstacles:
-    print(obj)
--- perception/ransac.py
+++ perception/ransac.py
@@ -7,7 +7,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from simulation.terrain_generator import generer_terrain
 
 
-THRESHOLD = 0.025
+THRESHOLD = 0.08
 
 def ransac(points, threshold):
     plane1 = pyrsc.Plane()
