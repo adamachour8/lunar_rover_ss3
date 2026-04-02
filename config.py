0ce1c84681101e4269ebc086789f99972254d8dc
Olivier Perreault <nullosperreault@gmail.com>
2026-04-02 11:09:54 -0400

    Modifs aux valeurs de config

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ DBSCAN_EPS           = 0.25   # Distance max entre deux points du même cluster
 DBSCAN_MIN_SAMPLES   = 8      # Nombre minimum de points pour former un cluster
 
 # --- FILTRATION ---
-FILTRE_HAUTEUR_MIN   = 0.05   # 5cm  — en dessous = bruit, ignoré
-FILTRE_HAUTEUR_MAX   = 0.50   # 50cm — en dessous = objet d'intérêt, au dessus = obstacle
+FILTRE_HAUTEUR_MIN   = 0.045   # 5cm  — en dessous = bruit, ignoré
+FILTRE_HAUTEUR_MAX   = 0.505   # 50cm — en dessous = objet d'intérêt, au dessus = obstacle
 FILTRE_DISTANCE_MAX  = 8.0    # 8m   — ton nuage va jusqu'à ~7m en diagonale, 5m était trop petit
 
 # --- TRIANGULATION ---
@@ -20,9 +20,9 @@ TRIANG_OUTLIER_DIST_MAX  = 0.2   # si le voisin le plus proche est à plus de 20
 
 # --- A* PATHFINDING ---
 ASTAR_RESOLUTION     = 0.10   # Taille cellule grille (m) — descendre = plus précis mais plus lent
-ASTAR_RAYON_ROVER    = 0.20   # Rayon du rover (m) — À AJUSTER selon specs réelles
-ASTAR_RAYON_INFLATION= 0.35   # Doit toujours être >= ASTAR_RAYON_ROVER
-ASTAR_SCAN_DISTANCE  = 0.50   # Distance à laquelle le rover s'arrête pour scanner (m)
+ASTAR_RAYON_ROVER    = 0.25   # Rayon du rover (m) — À AJUSTER selon specs réelles
+ASTAR_RAYON_INFLATION= 0.40   # Doit toujours être >= ASTAR_RAYON_ROVER
+ASTAR_SCAN_DISTANCE  = 0.70   # Distance à laquelle le rover s'arrête pour scanner (m)
 ASTAR_Z_SOL          = 3.10   # Hauteur Z du sol (m) — pour filtrer les points hors-sol
 
 # --- FICHIERS ---
